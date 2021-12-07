36d8c5dd0e4b99b3fd21cb5e6fbdf59ac8fd61d7
George Grammenidis <grammenidis@yahoo.gr>
2021-12-07 12:00:59 +0200

    basic form stracture created

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function App() {
+  var id = "searchText_" + uuidv4();
   return (
     <>
-      <h1>Hello world!</h1>
+      <h1 className="sr-only">Auto suggester</h1>
+      <form className="as-form">
+        <label htmlFor={id} className="as-label">
+          Search
+        </label>
+        <input
+          id={id}
+          className="as-input"
+          type="text"
+          name="searchText"
+        ></input>
+        <button className="as-button">SEARCH</button>
+      </form>
     </>
   );
 }
